b2c58270b362bcad553757b27e39ef208e9d552f
Juliana Joinville <joinville.juliana@gmail.com>
2014-11-29 17:21:32 +0100

    Create result.sh

---
+++ result.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+for i in $(find ./Shell/ -name "*f*");
+  	do countLine=$(cat $i | wc -l) ; 
+		echo $countLine ; 
+		if [ $countLine -gt 10 ] ; 
+		then  
+			echo $i;  
+ 			mv $i $(echo $i | sed 's/f/g/g') ;
+			resultLine=$(find -name "*f*");
+		fi;
+ 	done;
+
